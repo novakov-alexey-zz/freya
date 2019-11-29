a5982572c2e37417fa5c8dc3a1b7e0cf4bf9c74b
Alexey Novakov <novakov.alex@gmail.com>
2019-11-29 23:12:47 +0100

    improve test

--- src/test/scala/io/github/novakovalexey/k8soperator/CrdOperatorTest.scala
+++ src/test/scala/io/github/novakovalexey/k8soperator/CrdOperatorTest.scala
@@ -1,14 +1,13 @@
 package io.github.novakovalexey.k8soperator
 
 import cats.effect.{ConcurrentEffect, ContextShift, IO, Sync, Timer}
-import com.typesafe.scalalogging.LazyLogging
 import io.fabric8.kubernetes.api.model.ObjectMeta
 import io.fabric8.kubernetes.api.model.apiextensions.CustomResourceDefinition
+import io.fabric8.kubernetes.client.Watcher.Action
 import io.fabric8.kubernetes.client.dsl.Watchable
 import io.fabric8.kubernetes.client.{Watch, Watcher}
-import io.github.novakovalexey.k8soperator.common.crd.InfoClass
-import io.github.novakovalexey.k8soperator.common.watcher.WatchMaker.ConsumerSignal
-import io.github.novakovalexey.k8soperator.common.watcher.{CrdWatcherContext, CustomResourceWatcher}
+import io.github.novakovalexey.k8soperator.watcher.WatcherMaker.{Consumer, ConsumerSignal}
+import io.github.novakovalexey.k8soperator.watcher.{CrdWatcherContext, CustomResourceWatcher, InfoClass}
 import org.scalacheck.{Arbitrary, Gen}
 import org.scalatest.concurrent.Eventually
 import org.scalatest.time.{Millis, Seconds, Span}
@@ -41,30 +40,33 @@ class CrdOperatorTest extends PropSpec with Matchers with Eventually with Checke
     (watchable, singleWatcher)
   }
 
-  implicit def crd[F[_]: ConcurrentEffect, T](
+  implicit def crdWatch[F[_]: ConcurrentEffect, T](
     implicit watchable: Watchable[Watch, Watcher[InfoClass[T]]]
   ): CrdWatchMaker[F, T] =
     (context: CrdWatcherContext[F, T]) =>
       new CustomResourceWatcher(context) {
-        override def watch: F[(Watch, ConsumerSignal[F])] =
+        override def watch: F[(Consumer, ConsumerSignal[F])] =
           registerWatcher(watchable)
     }
 
   implicit def crdDeployer[F[_]: Sync, T]: CrdDeployer[F, T] =
     (_, _: CrdConfig[T], _: Option[Boolean]) => Sync[F].pure(new CustomResourceDefinition())
 
-  class KrbController[F[_]](implicit F: ConcurrentEffect[F]) extends Controller[F, Krb2] with LazyLogging {
-    val addEvents: mutable.Set[(Krb2, Metadata)] = mutable.Set.empty
+  class KrbController[F[_]](implicit F: ConcurrentEffect[F]) extends Controller[F, Krb2] {
+    val addEvents: mutable.Set[(Action, Krb2, Metadata)] = mutable.Set.empty
+    var initialized: Boolean = false
 
     override def onAdd(krb: Krb2, meta: Metadata): F[Unit] =
-      F.delay(addEvents += ((krb, meta)))
+      F.delay(addEvents += ((Action.ADDED, krb, meta)))
 
     override def onDelete(krb: Krb2, meta: Metadata): F[Unit] =
-      F.delay(logger.info(s"Krb deleted: $krb, $meta"))
+      F.delay(addEvents += ((Action.DELETED, krb, meta)))
 
-    override def onModify(entity: Krb2, metadata: Metadata): F[Unit] = super.onModify(entity, metadata)
+    override def onModify(krb: Krb2, meta: Metadata): F[Unit] =
+      F.delay(addEvents += ((Action.MODIFIED, krb, meta)))
 
-    override def onInit(): F[Unit] = super.onInit()
+    override def onInit(): F[Unit] =
+      F.delay(this.initialized = true)
   }
 
   property("handle different events") {
@@ -86,13 +88,16 @@ class CrdOperatorTest extends PropSpec with Matchers with Eventually with Checke
           t.printStackTrace()
       }
 
-    forAll(InfoClass.gen[Krb2]) { ic =>
+    controller.initialized should ===(true)
+
+    forAll(WatcherAction.gen, InfoClass.gen[Krb2]) { (action, resource) =>
       //when
-      singleWatcher.foreach(_.eventReceived(Watcher.Action.ADDED, ic))
+      singleWatcher.foreach(_.eventReceived(action, resource))
+      val meta = Metadata(resource.getMetadata.getName, resource.getMetadata.getNamespace)
 
       //then
       eventually {
-        controller.addEvents should contain((ic.getSpec, Metadata(ic.getMetadata.getName, ic.getMetadata.getNamespace)))
+        controller.addEvents should contain((action, resource.getSpec, meta))
       }
     }
 
@@ -125,5 +130,10 @@ class CrdOperatorTest extends PropSpec with Matchers with Eventually with Checke
         ic.setMetadata(meta)
         ic
       }
+  }
+
+  object WatcherAction {
+    def gen: Gen[Action] =
+      Gen.oneOf(Action.ADDED, Action.DELETED, Action.MODIFIED)
   }
 }
