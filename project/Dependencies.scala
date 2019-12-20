d41b0471ee162ba8e9e39f42eab2f0489f7d2a8b
Scala Steward <me@scala-steward.org>
2019-12-20 08:32:44 +0100

    Update scalacheck to 1.14.3

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -16,7 +16,7 @@ object Dependencies extends AutoPlugin {
       val scalaLoggingVersion              = "3.9.2"
       val scalaTestVersion                 = "3.1.0"
       val scalaTestCheckVersion            = "3.1.0.0-RC2"
-      val scalaCheckVersion                = "1.14.2"
+      val scalaCheckVersion                = "1.14.3"
       val scalaJsonSchemaV                 = "0.2.2"
     }
 
