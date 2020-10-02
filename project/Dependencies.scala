be5f6f072c7a2a7338e16d12fc370d5401e0b9e1
Alexey Novakov <novakov.alex@gmail.com>
2020-10-02 17:47:00 +0200

    downgrade jackson dependency

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -13,7 +13,7 @@ object Dependencies extends AutoPlugin {
       val circeYamlVersion                 = "0.13.1"
       val circeExtrasVersion               = "0.12.2"
       val fabric8K8sVersion                = "4.12.0"
-      val jacksonScalaVersion              = "2.11.3"
+      val jacksonScalaVersion              = "2.11.2"
       val jacksonJsonSchemaV               = "1.0.39"
       val logbackClassicVersion            = "1.3.0-alpha4"
       val scalaLoggingVersion              = "3.9.2"
